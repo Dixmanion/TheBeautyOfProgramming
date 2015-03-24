83a0f4b78b9aa4cfea22fe286384fd01835514e1
Eureka Liu <xing_ruo@163.com>
2015-03-24 16:31:36 +0800

    [add]

--- ProblemSolving/src/nju/edu/cn/SelectionSort.java
+++ ProblemSolving/src/nju/edu/cn/SelectionSort.java
@@ -31,12 +31,44 @@ public class SelectionSort {
 			}
 			
 		}
-		
 	}
+	
+	/**
+	 * 递归版本
+	 * @param array
+	 * @param start
+	 */
+	public void selectinSort(int[] array, int start) {
+		
+		//防御式编程
+		if (array == null || array.length == 0 || array.length == 1) { return; }
+		
+		//递归终止条件
+		if (start == array.length - 1) { return; }
+		
+		int min = array[start];
+		int minIndex = start;
+		
+		for (int i = start; i < array.length; i++) {	
+			if (array[i] < min) {
+				min = array[i];
+				minIndex = i;
+			}
+		}
+		
+		if (minIndex != start) {
+			array[minIndex] = array[start];
+			array[start] = min;
+		}
+		
+		selectinSort(array, start+1);
+	}
+	
 	public static void main(String[] args) {
 		int[] array = {-4758, 32, 0 ,-1, -1, 438, 34, 6};
 		SelectionSort selectionSort = new SelectionSort();
-		selectionSort.selectionSort(array);
+		//selectionSort.selectionSort(array);
+		selectionSort.selectinSort(array, 0);
 		System.out.println(Arrays.toString(array));
 	}
 
